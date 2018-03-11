39604d37e875031963feffbf4ffe595cf257da60
Igor Kamyshev <garik.novel@gmail.com>
2018-03-11 15:12:13 +0700

    add tests for all rest methods

--- app/front/util/api/__test__/rest.spec.ts
+++ app/front/util/api/__test__/rest.spec.ts
@@ -12,9 +12,7 @@ import momentalizeEntity from '../momentalizeEntity'
 interface Entity {
     '@id': string
     '@type': string
-
     '@context'?: string
-
     id: number
 }
 
@@ -43,4 +41,40 @@ test('get list should returns collection of entities', () => {
 
             expect(collection.totalItems).toEqual(2)
         })
+})
+
+test('get should returns entity', () => {
+    const data = { '@id': '/api/tests/1', '@type': 'Test', id: 1 } as any
+
+    (axios as any).get.mockResolvedValue({ data })
+
+    return get(1)
+        .then(entity => expect(entity).toEqual(data))
+})
+
+test('post should returns entity', () => {
+    const data = { '@id': '/api/tests/1', '@type': 'Test', id: 1 } as any
+
+    (axios as any).post.mockResolvedValue({ data })
+
+    return post(data)
+        .then(entity => expect(entity).toEqual(data))
+})
+
+test('put should returns entity', () => {
+    const data = { '@id': '/api/tests/1', '@type': 'Test', id: 1 } as any
+
+    (axios as any).put.mockResolvedValue({ data })
+
+    return put(1, data)
+        .then(entity => expect(entity).toEqual(data))
+})
+
+test('del should returns true', () => {
+    const data = {} as any
+
+    (axios as any).delete.mockResolvedValue({ data })
+
+    return del(1)
+        .then(result => expect(result).toBe(true))
 })
