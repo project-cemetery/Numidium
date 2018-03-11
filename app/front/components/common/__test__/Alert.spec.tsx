801c8456466164779122d8ea5dbe70acb59e2fea
Igor Kamyshev <garik.novel@gmail.com>
2018-03-11 18:46:54 +0700

    add test for breadcrumbs

--- app/front/components/common/__test__/Alert.spec.tsx
+++ app/front/components/common/__test__/Alert.spec.tsx
@@ -3,6 +3,7 @@ import * as renderer from 'react-test-renderer'
 
 import Alert, { AlertType } from '../Alert'
 
+
 test('alert renders correctly', () => {
     const tree = renderer
         .create(<Alert type={AlertType.INFO} message={'Test message!'} />)
---
+++ app/front/components/common/__test__/Breadcrumbs.spec.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import * as renderer from 'react-test-renderer'
+
+import Breadcrumbs from '../Breadcrumbs'
+
+
+test('breadcrumbs renders correctly with items', () => {
+    const breadcrumbs = [ 'Home', 'Profile' ]
+
+    const tree = renderer
+        .create(<Breadcrumbs breadcrumbs={breadcrumbs} />)
+        .toJSON()
+
+    expect(tree).toMatchSnapshot()
+})
+
+test('breadcrumbs renders correctly withpout items', () => {
+    const tree = renderer
+        .create(<Breadcrumbs />)
+        .toJSON()
+
+    expect(tree).toMatchSnapshot()
+})
