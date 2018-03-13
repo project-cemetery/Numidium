6c220b6ef67b99baf56dda7afae965696b1b04c2
Igor Kamyshev <garik.novel@gmail.com>
2018-03-13 15:56:12 +0700

    Add test for loader and some refactoring

---
+++ app/front/components/common/__test__/Loader.spec.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import * as renderer from 'react-test-renderer'
+
+import Loader from '../Loader'
+
+
+test('`Loader` renders correctly with loading state', () => {
+    const tree = renderer
+        .create(
+            <Loader loading={true} error={false}>
+                <p>Content</p>
+            </Loader>
+        )
+        .toJSON()
+
+    expect(tree).toMatchSnapshot()
+})
+
+test('`Loader` renders correctly with error state', () => {
+    const tree = renderer
+        .create(
+            <Loader loading={false} error={true}>
+                <p>Content</p>
+            </Loader>
+        )
+        .toJSON()
+
+    expect(tree).toMatchSnapshot()
+})
+
+test('`Loader` renders correctly with content', () => {
+    const tree = renderer
+        .create(
+            <Loader loading={false} error={false}>
+                <p>Content</p>
+            </Loader>
+        )
+        .toJSON()
+
+    expect(tree).toMatchSnapshot()
+})
