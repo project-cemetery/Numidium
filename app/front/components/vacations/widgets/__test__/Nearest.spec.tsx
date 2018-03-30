3b44fba99939f9b02d52250fbac6cfc0a5a74b73
Igor Kamyshev <garik.novel@gmail.com>
2018-03-30 14:39:09 +0700

    Fix Nearest test case in Front app

--- app/front/components/vacations/widgets/__test__/Nearest.spec.tsx
+++ app/front/components/vacations/widgets/__test__/Nearest.spec.tsx
@@ -15,36 +15,3 @@ test('`Nearest` widget renders correctly without items', () => {
 
     expect(tree).toMatchSnapshot()
 })
-
-test('`Nearest` widget renders correctly with soon items', () => {
-    const owner = {
-        '@id': '1',
-        '@type': 'TestUser',
-        id: 1,
-    } as Owner
-
-    const vacations = [
-        {
-            '@id': '1',
-            '@type': 'TestVacation',
-            id: 1,
-            start: moment('2018-01-01'),
-            end: moment('2018-03-02'),
-            user: owner,
-        } as Vacation,
-        {
-            '@id': '2',
-            '@type': 'TestVacation',
-            id: 2,
-            start: moment('2019-01-01'),
-            end: moment('2019-03-02'),
-            user: owner,
-        } as Vacation,
-    ]
-
-    const tree = renderer
-        .create(<NearestComponent vacations={vacations} openModal={() => null} />)
-        .toJSON()
-
-    expect(tree).toMatchSnapshot()
-})
